b0edcae86c95a9efb8a7becd84296bcbb4350923
fnahon <flo.nahon@hotmail.fr>
2023-11-13 23:57:14 +0100

    Correction TopicRepository.findAll(Pageable pageable)

--- src/main/java/com/fleo/javaforum/repository/TopicRepository.java
+++ src/main/java/com/fleo/javaforum/repository/TopicRepository.java
@@ -2,11 +2,12 @@ package com.fleo.javaforum.repository;
 
 import com.fleo.javaforum.model.Topic;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TopicRepository extends CrudRepository<Topic, Long>, PagingAndSortingRepository<Topic, Long> {
-    Page<Topic> findAll();
+    Page<Topic> findAll(Pageable pageable);
 }
