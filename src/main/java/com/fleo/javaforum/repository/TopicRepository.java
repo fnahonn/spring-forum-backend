441b34a55d5e3b7c10b42ae053a60d5d0d8e41de
fnahon <flo.nahon@hotmail.fr>
2023-11-13 23:30:35 +0100

    Add bean annotation @Repository to TopicRepository

--- src/main/java/com/fleo/javaforum/repository/TopicRepository.java
+++ src/main/java/com/fleo/javaforum/repository/TopicRepository.java
@@ -4,7 +4,9 @@ import com.fleo.javaforum.model.Topic;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TopicRepository extends CrudRepository<Topic, Long>, PagingAndSortingRepository<Topic, Long> {
     Page<Topic> findAll();
 }
