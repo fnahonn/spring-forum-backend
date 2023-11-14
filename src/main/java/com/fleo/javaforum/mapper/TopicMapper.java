2b4c5f7834a7ed890ae0fe11c2f5336ff61e6aba
fnahon <flo.nahon@hotmail.fr>
2023-11-14 07:53:10 +0100

    Initial TopicMapper

---
+++ src/main/java/com/fleo/javaforum/mapper/TopicMapper.java
@@ -0,0 +1,18 @@
+package com.fleo.javaforum.mapper;
+
+import com.fleo.javaforum.model.Topic;
+import com.fleo.javaforum.payload.response.TopicResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TopicMapper {
+
+    @Mapping(target="id", source="id")
+    @Mapping(target="name", source="name")
+    @Mapping(target="content", source="content")
+    @Mapping(target="solved", source="solved")
+    @Mapping(target="createdAt", source="createdAt")
+    @Mapping(target="updatedAt", source="updatedAt")
+    TopicResponse toResponse(Topic topic);
+}
