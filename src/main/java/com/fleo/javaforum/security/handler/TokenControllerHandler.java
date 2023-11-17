e7eb8eca5efbd70f71a099beed81bbc7c334311d
fnahon <flo.nahon@hotmail.fr>
2023-11-17 13:55:21 +0100

    Init TokenControllerHandler

---
+++ src/main/java/com/fleo/javaforum/security/handler/TokenControllerHandler.java
@@ -0,0 +1,32 @@
+package com.fleo.javaforum.security.handler;
+
+import com.fleo.javaforum.payload.response.ErrorResponse;
+import com.fleo.javaforum.security.exception.TokenException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Instant;
+import java.util.Set;
+
+@RestControllerAdvice
+public class TokenControllerHandler {
+
+    @ExceptionHandler(TokenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<ErrorResponse> handleRefreshTokenException(TokenException ex, WebRequest request) {
+        final ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(Instant.now())
+                .errors(Set.of("Invalid Token"))
+                .status(HttpStatus.FORBIDDEN.value())
+                .message(ex.getMessage())
+                .path(request.getDescription(false))
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(errorResponse);
+    }
+}
