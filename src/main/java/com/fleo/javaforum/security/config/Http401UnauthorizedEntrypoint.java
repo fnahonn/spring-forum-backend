823ad81b3a2cafb59c0ac1eec46665051f1356a4
fnahon <flo.nahon@hotmail.fr>
2023-11-18 20:24:59 +0100

    Init Http401UnauthorizedEntrypoint

---
+++ src/main/java/com/fleo/javaforum/security/config/Http401UnauthorizedEntrypoint.java
@@ -0,0 +1,42 @@
+package com.fleo.javaforum.security.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fleo.javaforum.payload.response.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Set;
+
+public class Http401UnauthorizedEntrypoint implements AuthenticationEntryPoint {
+
+    private Logger log = LoggerFactory.getLogger(Http401UnauthorizedEntrypoint.class);
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized error : {}", authException.getMessage());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ErrorResponse body = ErrorResponse.builder()
+                .status(HttpServletResponse.SC_UNAUTHORIZED)
+                .errors(Set.of("Unauthorized"))
+                .timestamp(Instant.now())
+                .message(authException.getMessage())
+                .path(request.getServletPath())
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+}
