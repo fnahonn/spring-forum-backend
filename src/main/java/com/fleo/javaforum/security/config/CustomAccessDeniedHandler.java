9c6031bb0223ec2812276bfdb93baabdf8e075de
fnahon <flo.nahon@hotmail.fr>
2023-11-18 20:25:20 +0100

    Init CustomAccessDeniedHandler

---
+++ src/main/java/com/fleo/javaforum/security/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,42 @@
+package com.fleo.javaforum.security.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fleo.javaforum.payload.response.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Set;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private Logger log = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("Access denied error : {}", accessDeniedException.getMessage());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        ErrorResponse body = ErrorResponse.builder()
+                .status(HttpServletResponse.SC_FORBIDDEN)
+                .errors(Set.of("Forbidden"))
+                .timestamp(Instant.now())
+                .message(accessDeniedException.getMessage())
+                .path(request.getServletPath())
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+}
