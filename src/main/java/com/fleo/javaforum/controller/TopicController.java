606f4c88bd0ffb32e9fdcb13c8857d0a52072046
fnahon <flo.nahon@hotmail.fr>
2023-11-19 12:47:22 +0100

    First version of TopicController

--- src/main/java/com/fleo/javaforum/controller/TopicController.java
+++ src/main/java/com/fleo/javaforum/controller/TopicController.java
@@ -1,2 +1,50 @@
-package com.fleo.javaforum.controller;public class TopicController {
+package com.fleo.javaforum.controller;
+
+import com.fleo.javaforum.payload.request.TopicRequest;
+import com.fleo.javaforum.payload.response.TopicResponse;
+import com.fleo.javaforum.service.TopicService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/forum")
+public class TopicController {
+
+    private static final int ELEMENTS_PER_PAGE = 10;
+    @Autowired
+    private TopicService topicService;
+
+    @GetMapping(value = "/topics", name = "topic.index")
+    public ResponseEntity<Iterable<TopicResponse>> index(@RequestParam(name = "p", defaultValue = "0") final int page) {
+        Iterable<TopicResponse> response = topicService.findAllTopics(page, ELEMENTS_PER_PAGE);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/topic/{id}", name = "topic.show")
+    public ResponseEntity<TopicResponse> show(@PathVariable(name = "id") final long topicId) {
+        TopicResponse response = topicService.findTopicById(topicId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = "/topic", name = "topic.new")
+    public ResponseEntity<TopicResponse> create(@Valid @RequestBody TopicRequest request, Authentication auth) {
+        TopicResponse response = topicService.createTopic(request, auth);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/topic/{id}", name = "topic.update")
+    public ResponseEntity<TopicResponse> update(@PathVariable(name = "id") final long id,
+                                                @Valid @RequestBody TopicRequest request) {
+        TopicResponse response = topicService.updateTopic(request, id);
+        return ResponseEntity.ok(response);
+    }
+    @DeleteMapping(value = "/topic/{id}", name = "topic.delete")
+    public ResponseEntity<String> delete(@PathVariable(name = "id") final long id) {
+        topicService.deleteTopic(id);
+        return ResponseEntity.ok(String.format("Topic %d has been deleted successfully", id));
+    }
 }
